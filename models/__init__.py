e2e1dad0a66e43d67a5dba5dc98c569cf6bd24d2
AustinRado <radoaussy@gmail.com>
2023-11-25 18:02:21 +0300

    init file modifications

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,30 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+from os import getenv
 
+from models.state import State
+from models.city import City
+from models.user import User
+from models.review import Review
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.place import Place
 
-storage = FileStorage()
+"""Dict that maps class names"""
+classes = {"User": User, "BaseModel": BaseModel,
+           "Place": Place, "State": State,
+           "City": City, "Amenity": Amenity,
+           "Review": Review}
+
+"""get storage type from env variable"""
+storage_type = getenv("HBNB_TYPE_STORAGE")
+
+if storage_type == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+
+"""Reload the data from storage (either database or file-based)"""
 storage.reload()
