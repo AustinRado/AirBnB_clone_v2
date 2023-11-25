e40fe1cc9ca48f4c2724ac067696182dc3971021
AustinRado <radoaussy@gmail.com>
2023-11-25 17:40:27 +0300

    db management

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,22 +1,64 @@
 #!/usr/bin/python3
 """New storage engine"""
 try:
-    from models.base_model import Base
-    from models.city import City
+    from os import getenv
     from sqlalchemy import create_engine
     from sqlalchemy.orm import sessionmaker, scoped_session
-    from os import getenv
+    from models.base_model import BaseModel, Base
+    from models.city import City
+    from models.amenity import Amenity
+    from models.place import Place
+    from models.state import State
+    from models.review import Review
+    from models.user import User
 except ImportError as e:
     print(f'Import error', e)
+
+"""Get environment variables"""
+database = getenv("HBNB_MYSQL_DB")
+user = getenv("HBNB_MYSQL_USER")
+host = getenv("HBNB_MYSQL_HOST")
+password = getenv("HBNB_MYSQL_PWD")
+hbnb_env = getenv("HBNB_ENV")
+
+"""Define a dictionary that maps class names to their corresponding classes"""
+classes = {"State": State, "City": City, "User": User,
+           "Place": Place, "Review": Review, "Amenity": Amenity}
 
 
 class DBStorage:
     """
-    
+    A class for managing database storage.
     """
     __engine = None
     __session = None
 
     def __init__(self):
-        self.__engine
+        """
+        Intitialise db instance
+        """
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
+                                      user,password,host,database), pool_pre_ping=True)
 
+        """If running in test env drop all tables"""
+        if hbnb_env == 'test':
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """query database session
+        RETURN: dict
+            key = <class-name>.<object-id>
+            value = object
+        """
+        db_obj = {}
+        if cls:
+            if type(cls) is str and cls in classes:
+                for obj in self.__session.query(classes[cls]).all():
+                    key = str(obj.__class__.__name__) + "." + str(obj.id)
+                    db_obj[key] = obj
+        else:
+            for class_name, class_type in classes.item():
+                for obj in self.__session.query(class_type).all():
+                    key = str(class_type.__name__) + "." + str(obj.id)
+                    db_obj[key] = obj
+        return db_obj
